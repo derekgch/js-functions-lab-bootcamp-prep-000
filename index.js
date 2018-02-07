cdcc55971245a3c0ff9f39dbf449c0e6b06b34b6
derek chen <derek.g.chen@gmail.com>
2018-02-07 22:45:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
+
+
+function holidayCountdown(holiday, days){
+  returns "It's ${days} days until ${holiday}!"
+
+}
