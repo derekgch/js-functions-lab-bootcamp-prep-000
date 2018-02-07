4fca94e01297d4799d975f6e6c684f2f5e5dbcff
derek chen <derek.g.chen@gmail.com>
2018-02-07 22:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidays(){
 function happyHolidaysTo (name) {
   var temp= name
   console.log(temp)
-  console.log('${temp}')
+  console.log(`${temp}`)
   var a = 5;
   var b = 10;
   console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
