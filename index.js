55a9f9c84af3b30a108be10062332c3014f54fe0
derek chen <derek.g.chen@gmail.com>
2018-02-07 23:00:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 // write your code below!
-happyHolidaysTo("yeaho");
-
 function happyHolidays(){
   return "Happy holidays!"
 }
 
 function happyHolidaysTo (name) {
-  var temp= name
-  console.log(temp)
-  console.log(`${temp}`)
-  var a = 5;
-  var b = 10;
-  console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`);
-  return "Happy holidays, ${name}!"
+  return `Happy holidays, ${name}!`
 }
 
 
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return `Happy ${holiday}, ${name}!`
 }
 
 
 function holidayCountdown(holiday, days){
-  return "It's ${days} days until ${holiday}!"
+  return `It's ${days} days until ${holiday}!`
 
 
 }
